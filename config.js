4b544b00329ddfe85e03dfbc846672b1f39d1e23
Priit Kallas <kallaspriit@gmail.com>
2016-03-18 14:58:01 +0200

    Implemented new bunyan based logger.

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+import bunyan from 'bunyan';
+import BunyanSlack from 'bunyan-slack';
+
 export const cumulocity = {
 	apiUrl: 'https://telia.cumulocity.com'
 };
@@ -6,7 +9,37 @@ export const server = {
 	port: 80
 };
 
+export const slack = {
+	url: 'https://hooks.slack.com/services/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'
+};
+
+export const logger = {
+	name: 'cumulocity',
+	serializers: {
+		...bunyan.stdSerializers
+	},
+	streams: [{
+		stream: process.stdout,
+		level: 'info'
+	}, {
+		type: 'rotating-file',
+		path: 'logs/log.txt',
+		period: '1d',
+		count: 3,
+		level: 'info'
+	}, {
+		stream: new BunyanSlack({
+			webhook_url: slack.url, // eslint-disable-line camelcase
+			channel: '#test',
+			username: 'Cumulocity.Error'
+		}),
+		level: 'error'
+	}]
+};
+
 export default {
 	cumulocity,
-	server
+	server,
+	slack,
+	logger
 };
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-import { server as config } from './config';
+import serveStatic from 'serve-static';
+import config from './config';
 import Server from './lib/server/Server';
-import logger from './lib/log';
+import logger from './lib/logger';
 
-const log = logger.get('server');
+const log = logger.get(__filename, { env: 'dev' });
 
-log('bootstrapping');
+log('bootstrapping', 123, { 'a': 1 });
+
 
 const server = new Server();
 
-server.performDefaultSetup();
+server.setupDefault();
+
+// serve static files from apps
+server.app.use(serveStatic('apps', {
+	index: 'index.html'
+}));
 
 server.get('/test', (req, res) => {
 	res.send('test response');
 });
 
-server.start(config.port);
+server.start(config.server.port);
